32442bf3b6f4aa5f4d0b158b49697d849bdee345
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-15 15:05:10 +0000

    Deploy ktweeden/personal_site to github.com/ktweeden/personal_site.git:gh-pages

---
+++ build/template_render.js
@@ -0,0 +1,34 @@
+const contentList = require("../templates/content_list.json")
+const navList = require("../templates/navigation.json")
+const nunjucks = require("nunjucks")
+const fs = require("fs")
+const path = require("path")
+
+nunjucks.configure((path.join(__dirname, "../templates")), {
+  autoescape: true,
+})
+
+function renderBlog() {
+  for (let content of contentList) {
+    const contentPath = `${content.url}.njk`
+    const fileContent = (nunjucks.render(contentPath, {navList: navList, contentList: contentList}))
+    const filePath = `./${content.url}.html`
+    fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
+  }
+}
+
+function renderIndex() {
+  const fileContent = (nunjucks.render("index.njk", {navList: navList, contentList: contentList}))
+  const filePath = ("./index.html")
+  fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
+}
+
+function renderAboutMe() {
+  const fileContent = (nunjucks.render("about_me.njk", {navList: navList}))
+  const filePath = ("./about_me.html")
+  fs.writeFile(filePath, fileContent, (err) => {if (err) throw err})
+}
+
+renderBlog()
+renderIndex()
+renderAboutMe()
